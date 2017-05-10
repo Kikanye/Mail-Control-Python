379f1ad42f679220b71e3e0a929eac34cb264588
Kikanye <Kikanye@users.noreply.github.com>
2017-05-10 09:10:57 -0500

    Attachment Functionality Aded
    Now downloads and saves the email attachments if present and saves them to a folder on the desktop with the appropriate file type.

--- MailProcessor.py
+++ MailProcessor.py
@@ -1,29 +1,48 @@
 import win32com.client
 import pythoncom
+import os
 import email
 import re
+import json
+#import urllib2
 
-class Handler_Class(object):
+count=1
+
+class HandlerClass(object):
 
     def OnNewMailEx(self, receivedItemsIDs):
         for Id in receivedItemsIDs.split(","):
             mail = outlook.Session.GetItemFromID(Id)
+
             print("Subject:  "+ mail.Subject)
             Body = mail.Body
+
             #Body=str(mail.Body.encode('ascii', 'ignore'))
-            Actual_Body=''
-            for letter in Body:
-                if letter!='\r':
-                    Actual_Body+=letter
+            #Actual_Body=''
+            #for letter in Body:
+                #if letter!='\r':
+                #Actual_Body+=letter
 
-
-
-            print("Body:  "+Actual_Body)
+            print("Body:  "+Body)
+            global count
+            if mail.Attachments:
+                while (os.path.isdir('C:\\Users\\rnchris\\Desktop\\Attachments'+str(count))):
+                    count+=1
+                os.makedirs('C:\\Users\\rnchris\\Desktop\\Attachments'+str(count))
+                #os.chdir('C:\\Users\\rnchris\\Desktop\\Attachments')
+                for att in mail.Attachments:
+                    attachmentType=att.FileName
+                    attachmentType=attachmentType.split('.')[1]
+                    att.SaveAsFile('C:\\Users\\rnchris\\Desktop\\Attachments'+str(count)+'\\Download'+str(count)+'.'+attachmentType)
+                    count+=1
+                    print('Worked!')
             #subject = mail.subject
             #command = re.search(r"%(.*?)%", subject).group(1)
             #print(command)
 
 
-outlook = win32com.client.DispatchWithEvents("Outlook.Application", Handler_Class)
+outlook = win32com.client.DispatchWithEvents("Outlook.Application", HandlerClass)
 
+#outlook2=win32com.client.Dispatch("Outlook.Application").GetNameSpace('MAPI')
+#checkAndDownLoadAttachments(outlook2)
 pythoncom.PumpMessages()
